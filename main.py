1125aaa32a662471c4cd13b50c64ff68c8ea7926
okn-yu <haruka.koishikawa@gmail.com>
2019-07-14 16:48:30 +0900

    updated.

--- main.py
+++ main.py
@@ -10,11 +10,6 @@ import matplotlib.pyplot as plt
 def max_feature(features):
 
     # 全512枚の特徴マップから活性値が最大値を取得する
-    print("max_feature")
-
-    print(features.shape)
-    print(type(features)) # <class 'torch.Tensor'>
-
     map_numbers = features.shape[0]
 
     act_lst = []
@@ -53,8 +48,6 @@ def max_feature(features):
 
 def imshow(img):
     npimg = img.data.numpy()
-
-    # 最小値を0,最大値を255に正規化
     npimg = ((npimg - npimg.min()) * 255 / (npimg.max() - npimg.min())).astype(int)
 
     # 要素の順番を(RGB, H, W) から (H, W, RGB)に変更
@@ -62,78 +55,92 @@ def imshow(img):
     plt.imshow(npimg)
     plt.show()
 
+def visualize(model, x, num):
+    print("loop start")
+
+    count = 0
+
+    deconv_list = []
+    unpool_list = []
+    features_list = []
+
+    for layer in model.features:
+
+        if isinstance(layer, torch.nn.Conv2d):
+            # print("Conv2d...%s" % layer)
+            B, H, W, C = x.shape
+            x = layer(x)
+            deconvLayer = nn.ConvTranspose2d(layer.out_channels, H, layer.kernel_size, layer.stride, layer.padding)
+
+            # print("deConv2d...%s" % deconvLayer)
+            deconvLayer.weight = layer.weight
+            deconv_list.append(deconvLayer)
+
+        if isinstance(layer, torch.nn.ReLU):
+            x = layer(x)
+            deconv_list.append(layer)
+
+        if isinstance(layer, torch.nn.MaxPool2d):
+            # print("MaxPool2d...%s" % layer)
+            x, index = layer(x)
+            unpool_list.append(index)
+            unpoolLayer = torch.nn.MaxUnpool2d(kernel_size=layer.kernel_size, stride=layer.stride,
+                                               padding=layer.padding)  # , dilation=1, ceil_mode=False))
+            # print("MaxUnpool2d...%s" % unpoolLayer)
+            deconv_list.append(unpoolLayer)
+
+        features_list.append(x.clone())
+
+        count += 1
+        if count == num:
+            break
+
+    print("loop end")
+
+    x = max_feature(x[0])
+    y = x.unsqueeze_(0)
+
+    _visualize(deconv_list, y, unpool_list)
+
+def _visualize(deconv_list, y, unpool_list):
+    for layer in reversed(deconv_list):
+        if isinstance(layer, nn.MaxUnpool2d):
+            y = layer(y, unpool_list.pop())
+        else:
+            y = layer(y)
+
+    imshow(y[0])
+
+
 # Read image
-img = cv2.imread("./data/plane.jpg")#, cv2.IMREAD_COLOR)
+img = cv2.imread("./data/cat.jpg")#, cv2.IMREAD_COLOR)
 img = cv2.resize(img, (224, 224))
 
 transform = transforms.Compose([
-    transforms.ToTensor(),
-    transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225])
+    transforms.ToTensor()#,
+    #transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225])
 ])
 
 img = transform(img)
 img.unsqueeze_(0)
 
 x = img
-deconv_list = []
-unpool_list = []
+model = models.vgg19(pretrained=True).eval()
 
-print("start loop")
+conv2d_list = []
 
-num = 0
-
-model = models.vgg16(pretrained=True).eval()
-
-for layer in model.features:
+# -> リスト内包で実装
+for i, layer in enumerate(model.features):
     if isinstance(layer, torch.nn.MaxPool2d):
         layer.return_indices = True
-
-for layer in model.features:
-
     if isinstance(layer, torch.nn.Conv2d):
-        print("Conv2d...%s" % layer)
-        B, H, W, C = x.shape
-        x = layer(x)
-        deconvLayer = nn.ConvTranspose2d(layer.out_channels, H, layer.kernel_size, layer.stride, layer.padding)
+        conv2d_list.append(i)
 
-        print("deConv2d...%s" % deconvLayer)
-        deconvLayer.weight = layer.weight
-        deconv_list.append(deconvLayer)
+print(conv2d_list)
 
-    if isinstance(layer, torch.nn.ReLU):
-        x = layer(x)
-        deconv_list.append(layer)
-
-    if isinstance(layer, torch.nn.MaxPool2d):
-        print("MaxPool2d...%s" % layer)
-        x, index = layer(x)
-        unpool_list.append(index)
-        unpoolLayer = torch.nn.MaxUnpool2d(kernel_size=layer.kernel_size, stride=layer.stride, padding=layer.padding)#, dilation=1, ceil_mode=False))
-        print("MaxUnpool2d...%s" % unpoolLayer)
-        deconv_list.append(unpoolLayer)
-
-    #num += 1
-    #if num == 28:
-    #    break
-
-print("loop end")
-
-x = max_feature(x[0])
-y = x.unsqueeze_(0)
-# y = x
-
-result = y.clone()
+for i in conv2d_list:
+    visualize(model, x, i)
 
 
-for layer in reversed(deconv_list):
-    if isinstance(layer, nn.MaxUnpool2d):
-        y = layer(y, unpool_list.pop())
-        print("unpool_list length: %s" % len(unpool_list))
-    else:
-        y = layer(y)
-
-imshow(y[0])
-
-plt.savefig('result.jpg')
 
 
